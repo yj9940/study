6985609764c859925628128b0cdbc80f95e374b4
yj9940 <786436456@qq.com>
2017-07-12 20:01:53 +0800

    浮点型不精确计算

--- c03/com.c
+++ c03/com.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 int main()
 {
-	int num1,num2,rs;
+	float num1,num2;
 	char ch;
 	printf("请输入一个算数式");
-	scanf("%d%c%d",&num1,&ch,&num2);
+	scanf("%f%c%f",&num1,&ch,&num2);
 	if('+' == ch)
 	{
-		printf("结果是:%d\n",num1+num2);
+		printf("结果是:%f\n",num1+num2);
 	}else if('-' == ch)
 	{	
-		printf("结果是:%d\n",num1-num2);
+		printf("结果是:%f\n",num1-num2);
 	}else if('*' == ch)
 	{
-		printf("结果是:%d\n",num1*num2);
+		printf("结果是:%f\n",num1*num2);
 	}else if('/' == ch)
 	{
-		printf("结果是:%d\n",num1/num2);
+		printf("结果是:%f\n",num1/num2);
 	}else {printf("没有结果\n");}
 
 		return 0;
